3f20f3b3b4ee7d2ae802978300d6b8a230bdc09b
Maslov Roman <masl.roman97@gmail.com>
2025-09-12 18:46:53 +0900

    Initial commit

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import { Icon } from "../icon/Icon";
+
+export const Logo = () => {
+	return (
+		<a href="#">
+			<Icon iconId="logo" width="185" height="48" viewBox="0 0 185 48" />
+		</a>
+	);
+};
